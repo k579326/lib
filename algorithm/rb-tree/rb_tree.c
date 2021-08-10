61b347c4df3d25680ddee97acbb58ae7201b0ec1
DESKTOP-JOGO4PM\K <k579326@163.com>
2021-08-11 01:03:33 +0800

    [mod] 红黑树插入完成中...

--- algorithm/rb-tree/rb_tree.c
+++ algorithm/rb-tree/rb_tree.c
@@ -1,6 +1,7 @@
 
 #include "rb_tree.h"
 
+#include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -16,10 +17,13 @@ typedef struct _treenode
     struct _treenode* parent_;
     struct _treenode* left_;
     struct _treenode* right_;
-    bool              color_;       // true: red, false: black
+    char              color_;       // true: red, false: black
     PAIR              pair_;
 }TreeNode;
 
+#define rb_black    0
+#define rb_red      1
+#define rb_white    -1      // use for create root node only
 
 bool less(int l, int r)
 {
@@ -43,18 +47,22 @@ TreeNode* Find(TreeNode* rbtree, int key)
     return rbtree;
 }
 
-TreeNode* FindLastLessNode(TreeNode* rbtree, int key)
+TreeNode* LastLessOrEnd(TreeNode* rbtree, int key, bool* equal)
 {
-    if (!rbtree) {
-        return NULL;
-    }
+    // rbtree不能为空，以此确保该函数必返回一个节点
+    assert(rbtree);
 
     if (!less(rbtree->pair_.key, key)) {
+        if (!less(key, rbtree->pair_.key))
+        {
+            *equal = true;
+        }
+
         TreeNode* leftnode = rbtree->left_;
         if (!leftnode) {
-            return NULL;
+            return rbtree;  // 返回边界节点
         }
-        return FindLastLessNode(rbtree->left_, key);
+        return LastLessOrEnd(rbtree->left_, key, equal);
     }
     else
     {
@@ -63,26 +71,30 @@ TreeNode* FindLastLessNode(TreeNode* rbtree, int key)
         {
             return rbtree;
         }
-        return FindLastLessNode(rightnode, key);
+        return LastLessOrEnd(rightnode, key, equal);
     }
 
     // never to this
     return NULL;
 }
-TreeNode* FindFirstLargeNode(TreeNode* rbtree, int key)
+TreeNode* FirstLargeOrEnd(TreeNode* rbtree, int key, bool* equal)
 {
-    if (!rbtree) {
-        return NULL;
-    }
+    // rbtree不能为空，以此确保该函数必返回一个节点
+    assert(rbtree);
 
     if (!less(key, rbtree->pair_.key)) 
     {
+        if (!less(rbtree->pair_.key, key))
+        {
+            *equal = true;
+        }
+
         TreeNode* rightnode = rbtree->right_;
         if (!rightnode)
         {
-            return NULL;
+            return rbtree;  // 返回边界节点
         }
-        return FindFirstLargeNode(rightnode, key);
+        return FirstLargeOrEnd(rightnode, key, equal);
     }
     else
     {
@@ -90,7 +102,7 @@ TreeNode* FindFirstLargeNode(TreeNode* rbtree, int key)
         if (!leftnode) {
             return rbtree;
         }
-        return FindFirstLargeNode(rbtree->left_, key);
+        return FirstLargeOrEnd(rbtree->left_, key, equal);
     }
     // never to this
     return NULL;
@@ -122,9 +134,28 @@ static bool NodeEqual(const TreeNode* l, const TreeNode* r)
     return !NodeLess(l, r) && !NodeLess(r, l);
 }
 
+static TreeNode* CreateNewNode()
+{
+    TreeNode* newnode = (TreeNode*)malloc(sizeof(TreeNode));
+    if (!newnode) {
+        return NULL;
+    }
+    memset(newnode, 0, sizeof(TreeNode));
+    newnode->color_ = rb_red;
+    return newnode;
+}
+
+static void AddLeftNode(TreeNode* parent, TreeNode* left)
+{
+    parent->left_ = left;
+    left->parent_ = parent;
+}
+
+
 TreeNode* Insert(TreeNode* rbtree, PAIR* pair)
 {
     TreeNode* insert_pos = NULL;
+    TreeNode* new_node = NULL;
     if (!rbtree) {
         return NULL;
     }
@@ -133,17 +164,69 @@ TreeNode* Insert(TreeNode* rbtree, PAIR* pair)
         查找到一个合适的插入点，但是这个点不一定比pair大，可能等于，也可能小于
         尝试先判断，根据判断结果来决定把新节点链到左边还是右边，然后再调整平衡？
     */
-    insert_pos = FindFirstLargeNode(rbtree, pair->key, true);
+    bool flag_equal = false;
+    insert_pos = FirstLargeOrEnd(rbtree, pair->key, &flag_equal);
     
-    TreeNode* left = insert_pos->left_;
-    TreeNode* parent = insert_pos->parent_;
-    if (PairEqual(&insert_pos->pair_, pair))
-        return NULL;
-    if (left && PairEqual(&left->pair_, pair))
-        return NULL;
-    if (parent && PairEqual(&parent->pair_, pair))
+    // 获取过程中发现与pair相等的节点
+    if (flag_equal)
         return NULL;
     
+    new_node = CreateNewNode();
+    memcpy(&new_node->pair_, pair, sizeof(PAIR));
+    // insert_pos肯定没有右节点，
+    // 因为节点全部左倾斜，insert_pos又肯定是叶子节点
+    // 所以只判断是否存在左节点
+
+    // 左节点也不用判断了，一定没有
+    // insert_pos是第一个比pair大的节点，说明它没有左子树
+    if (insert_pos->left_)
+    {
+        // 顺序一定是 insert_pos > pair > insert_pos->left
+    }
+
+    if (insert_pos->color_ == rb_black) {
+        AddLeftNode(insert_pos, new_node);
+        return new_node;
+    }
+
+    // 现在insert_pos是红色的（红色不可能是根节点）
+    // insert_pos即可能是左节点，又可能是右节点
+    // 检测insert_pos如果没有右兄弟节点
+    if (!insert_pos->parent_->right_)
+    {
+        // 没有右兄弟节点，所以 pair < insert_pos < insert_pos->parent
+        // 旋转，使insert_pos变为父节点
+        TreeNode* parent = insert_pos->parent_;
+        TreeNode* gparent = insert_pos->parent_->parent_;
+        if (gparent) 
+        {
+            if (gparent->left_ == parent)
+            {
+                gparent->left_ = insert_pos;
+            }
+            else {
+                gparent->right_ = insert_pos;
+            }
+            insert_pos->parent_ = gparent;
+        }
+        insert_pos->right_ = parent;
+        parent->parent_ = insert_pos;
+        insert_pos->color_ = rb_black;
+        parent->color_ = rb_red;
+        AddLeftNode(insert_pos, new_node);
+        return new_node;
+    }
+    else
+    {// 右兄弟节点存在，分两种情况：1、insert_pos在右(左倾规则，有右必有左)；2、右兄弟存在，而insert_pos在左
+     // 无论这两种情况的哪种，insert_pos, insert_pos->parent, sibing of inser_pos
+     // 这三个节点必然是构成了一个4阶树中4节点，所以是一黑两红
+        
+     // 如果新插入的节点大于insert_pos的父节点，那么4个节点的大小关系如下：
+     // sibing < parent < newnode < sibing，否则 sibling < newnode < parent < sibing
+     // 根据4阶树扩展规则，升序排列第二位的节点将向上扩展
+
+    }
+
     // 所有元素都比pair小
     if (PairLess(&insert_pos->pair_, pair)) {
 
