86d04f6ce37bde466059836140cf9b855603f2b4
k579326 <k579326@163.com>
2022-07-28 19:55:42 +0800

    [mod] 优化升级代码执行效率统计的类，使打印结果支持显示层级关系，解决多线程对层级关系的影响

--- cpp_common/include/codeblockperformancecounter.h
+++ cpp_common/include/codeblockperformancecounter.h
@@ -13,6 +13,10 @@
 #include <unordered_set>
 #include <chrono>
 #include <mutex>
+#include <thread>
+#include <map>
+
+#include <assert.h>
 
 using namespace std::chrono;
 
@@ -21,21 +25,57 @@ namespace CodeBlockPerformanceCounter
     class BlockInfo
     {
     public:
+        uint64_t start_timepoint = 0;   // ...
         uint64_t invoke_times = 0;
-        uint64_t tot_time = 0;			// 程序运行期间，目标代码块的总耗时	(us）
-        uint64_t avg_time = 0;			// 目标代码块的平均耗时				(us)
-        uint64_t max_time = 0;			// 用时最多的一次记录				    (us)
+        uint64_t tot_time = 0;			// 程序运行期间，目标代码块的总耗时     (us)
+        uint64_t avg_time = 0;			// 目标代码块的平均耗时                 (us)
+        uint64_t max_time = 0;			// 用时最多的一次记录                   (us)
         uint64_t min_time = -1;			// 用时最少的一次记录				    (us)
         std::string blocktag = "";		// block tag
         std::string filename = "";		// 此block所属的文件名
         std::string funcname = "";		// 此block所属的函数名
         uint32_t linenumber = 0;		// block在文件中的行号
+        uint8_t     level = 0;          // 层级关系
 
         bool operator==(const BlockInfo& info) const
         {
             return info.filename == this->filename && info.funcname == this->funcname
                 && info.linenumber == this->linenumber;
         }
+    };
+
+
+    class _iN_cOunteR_CLasS;
+
+    class coUntErMGr
+    {
+    private:
+        coUntErMGr() {}
+        ~coUntErMGr() {}
+
+        _iN_cOunteR_CLasS* GetCurrentCounter() {
+            _iN_cOunteR_CLasS* ret = nullptr;
+            lock_.lock();
+            auto it = instance_list_.find(std::this_thread::get_id());
+            if (it != instance_list_.end())
+                ret = it->second.get();
+            else
+            {
+                auto rst = instance_list_.insert(std::make_pair(std::this_thread::get_id(), std::make_shared<_iN_cOunteR_CLasS>()));
+                ret = (_iN_cOunteR_CLasS*)(rst.first->second.get());
+            }
+            lock_.unlock();
+            return ret;
+        }
+
+    public:
+        static _iN_cOunteR_CLasS* GetInstance() {
+            static coUntErMGr mgr;
+            return mgr.GetCurrentCounter();
+        }
+    private:
+        std::map<std::thread::id, std::shared_ptr<_iN_cOunteR_CLasS>> instance_list_;
+        std::mutex lock_;
     };
 
 
@@ -58,67 +98,103 @@ namespace CodeBlockPerformanceCounter
         }
     };
 
-
     class _iN_cOunteR_CLasS
     {
     public:	
-        static _iN_cOunteR_CLasS* GetInstance()
+        _iN_cOunteR_CLasS() : level_(0)
         {
-            static _iN_cOunteR_CLasS _instance;
-            return &_instance;
+        }
+        ~_iN_cOunteR_CLasS()
+        {
+            Print();
         }
         
-        void RecordScope(const std::string& filename, const std::string& funcname, uint32_t linenum, 
-            const std::string tag, const microseconds& costtime)
+        void RecordScopeStart(const std::string& filename, const std::string& funcname, uint32_t linenum, 
+            const std::string tag, const microseconds& starttime)
         {
             BlockInfo localtmp;
             localtmp.funcname = funcname;
             localtmp.filename = filename;
             localtmp.linenumber = linenum;
             localtmp.blocktag = tag;
-            localtmp.invoke_times = 1;
+            localtmp.invoke_times = 0;
 
-            uint64_t time_interger = costtime.count();
+            localtmp.start_timepoint = starttime.count();
             
             std::lock_guard<std::mutex> autolock(lock_);
             auto rst_pair = block_list_.insert(localtmp);
+            BlockInfo* info = const_cast<BlockInfo*>(&(*rst_pair.first));
             if (!rst_pair.second)
             {
-                AdjustBlockInfo(const_cast<BlockInfo*>(&(*rst_pair.first)), time_interger);
+                if (info->start_timepoint != 0) {
+                    assert(false);
+                    printf("%s(%s): BlockMonitorEnd is missing? BlockMonitorStart need match a BlockMonitorEnd!\n", 
+                        __FILE__, __FUNCTION__);
+                    return;
+                }
+                info->start_timepoint = localtmp.start_timepoint;
             }
             else
             {
-                BlockInfo* info = const_cast<BlockInfo*>(&(*rst_pair.first));
-                info->avg_time = time_interger;
-                info->max_time = time_interger;
-                info->min_time = time_interger;
-                info->tot_time = time_interger;
+                // first insert success
+                info->level = level_++;
+                orderlist_.push_back(info);
             }
         }
+        void RecordScopeEnd(const std::string& filename, const std::string& funcname, uint32_t linenum,
+            const std::string tag, const microseconds& endtime)
+        {
+            BlockInfo localtmp;
+            localtmp.funcname = funcname;
+            localtmp.filename = filename;
+            localtmp.linenumber = linenum;
+            localtmp.blocktag = tag;
 
+            uint64_t interval = endtime.count();
+
+            std::lock_guard<std::mutex> autolock(lock_);
+            auto iterator = block_list_.find(localtmp);
+
+            //auto rst_pair = block_list_.insert(localtmp);
+            if (iterator != block_list_.end() && iterator->start_timepoint != 0)
+            {
+                BlockInfo* info = const_cast<BlockInfo*>(&(*iterator));
+                interval = interval - info->start_timepoint;
+                AdjustBlockInfo(info, interval);
+                info->start_timepoint = 0;
+                level_--;
+            }
+            else /* !rst_pair.second || rst_pair.first->start_timepoint == 0 */
+            {
+                // miss BlockMonitorBegin label?
+                assert(false);
+            }
+            return;
+        }
     private:
         void Print()
         {
             printf("total record:\n");
-            for (auto e : block_list_)
+            for (auto e : orderlist_)
             {
-                printf("----------\n");
-                printf("%*s%-15s", 2, "", "tag:");
-                printf("%s\n", e.blocktag.c_str());
-                printf("%*s%-15s", 2, "", "file:");
-                printf("%s\n", e.filename.c_str());
-                printf("%*s%-15s", 2, "", "function:");
-                printf("%s\n", e.funcname.c_str());
-                printf("%*s%-15s", 2, "", "times:");
-                printf("%llu\n", e.invoke_times);
-                printf("%*s%-15s", 2, "", "total time:");
-                printf("%llu ms\n", e.tot_time / 1000);
-                printf("%*s%-15s", 2, "", "avg time:");
-                printf("%llu us\n", e.avg_time);
-                printf("%*s%-15s", 2, "", "max time:");
-                printf("%llu us\n", e.max_time);
-                printf("%*s%-15s", 2, "", "min time:");
-                printf("%llu us\n", e.min_time);
+                int intent = 4 * (e->level + 1);
+                printf("%d%*s----------\n", e->level, intent, "");
+                printf("%*s+ %-15s", intent, "", "tag:");
+                printf("%s\n", e->blocktag.c_str());
+                printf("%*s+ %-15s", intent, "", "file:");
+                printf("%s\n", e->filename.c_str());
+                printf("%*s+ %-15s", intent, "", "function:");
+                printf("%s\n", e->funcname.c_str());
+                printf("%*s+ %-15s", intent, "", "times:");
+                printf("%llu\n", e->invoke_times);
+                printf("%*s+ %-15s", intent, "", "total time:");
+                printf("%llu ms\n", e->tot_time / 1000);
+                printf("%*s+ %-15s", intent, "", "avg time:");
+                printf("%llu us\n", e->avg_time);
+                printf("%*s+ %-15s", intent, "", "max time:");
+                printf("%llu us\n", e->max_time);
+                printf("%*s+ %-15s", intent, "", "min time:");
+                printf("%llu us\n", e->min_time);
                 printf("\n");
             }
         }
@@ -136,19 +212,12 @@ namespace CodeBlockPerformanceCounter
         }
 
     private:
-        _iN_cOunteR_CLasS()
-        {
-        }
-        ~_iN_cOunteR_CLasS() 
-        {
-            Print();
-        }
-        
         std::unordered_set<BlockInfo, Hasher, Cmper> block_list_;
+        std::vector<BlockInfo*> orderlist_;
         std::mutex lock_;
+        uint8_t level_;
     };
-    
-    
+
 };
 
 
@@ -161,14 +230,15 @@ namespace CodeBlockPerformanceCounter
 
 #define BlockMonitorBegin(tag)			\
 uint32_t lInENumBEr_##tag = __LINE__;	\
-auto __bloCkMoNiTOrstart_##tag = std::chrono::steady_clock::now().time_since_epoch()
+auto __bloCkMoNiTOrstart_##tag = std::chrono::steady_clock::now().time_since_epoch(); \
+coUntErMGr::GetInstance()->RecordScopeStart(__FILE__, __FUNCTION__, lInENumBEr_##tag, #tag, \
+duration_cast<microseconds>(__bloCkMoNiTOrstart_##tag))
 
 
 #define BlockMonitorEnd(tag)		\
-auto __iNtErvAl_##tag = std::chrono::steady_clock::now().time_since_epoch() - \
-__bloCkMoNiTOrstart_##tag; \
-_iN_cOunteR_CLasS::GetInstance()->RecordScope(__FILE__, __FUNCTION__, lInENumBEr_##tag, #tag, \
-duration_cast<microseconds>(__iNtErvAl_##tag))
+auto __bloCkMoNiTOreNd_##tag = std::chrono::steady_clock::now().time_since_epoch();\
+coUntErMGr::GetInstance()->RecordScopeEnd(__FILE__, __FUNCTION__, lInENumBEr_##tag, #tag, \
+duration_cast<microseconds>(__bloCkMoNiTOreNd_##tag))
 
 #else
     
